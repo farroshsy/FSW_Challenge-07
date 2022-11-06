7e166c53176d6d206f8900be80fb96b616da72ef
Farros <86004023+farroshsy@users.noreply.github.com>
2022-11-06 07:47:16 +0700

    Delete src directory

--- src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from "redux";
-
-import authReducer from "./authReducer";
-import carsReducer from "./carsReducer";
-
-export default combineReducers({
-    auth: authReducer,
-    cars: carsReducer,
-});
